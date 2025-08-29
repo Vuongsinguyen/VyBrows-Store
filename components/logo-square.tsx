0b41cd39c2cf3f6d6a0c7ed292cdb20778d7acdf
Nguyen Vuong <Vuongsinguyen@users.noreply.github.com>
2025-08-29 21:14:27 +0700

    fix: Correct logo size classes for 'sm' variant in LogoSquare component

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -8,14 +8,14 @@ export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
         'flex flex-none items-center justify-center bg-white dark:bg-black',
         {
           'h-[40px] w-[40px] rounded-xl': !size,
-          'h-[40px] w-[40px] rounded-lg': size === 'sm'
+          'h-[24px] w-[24px] rounded-lg': size === 'sm'
         }
       )}
     >
       <LogoIcon
         className={clsx({
           'h-[40px] w-[40px]': !size,
-          'h-[40px] w-[40px]': size === 'sm'
+          'h-[24px] w-[24px]': size === 'sm'
         })}
       />
     </div>
