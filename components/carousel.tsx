04ab2ee69ffedf7cc7266a0faa987aa041a188c0
Nguyen Vuong <Vuongsinguyen@users.noreply.github.com>
2025-08-29 20:26:38 +0700

    feat: Add carousel animation styles for improved visual presentation

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -2,6 +2,7 @@ import fs from 'fs';
 import type { Product } from 'lib/types';
 import Link from 'next/link';
 import path from 'path';
+import '../app/carousel.css';
 import { GridTileImage } from './grid/tile';
 
 export async function Carousel() {
